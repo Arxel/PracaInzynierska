9d7f492adfc7b2a9d61fcaeb32edbb528591994e
wojtekmaciej <wojtekmaciej@gmail.com>
2015-03-07 14:26:36 +0100

    Add 5 sec. fullscreen splash and next HomepageActivity

---
+++ Prototype_SmacznyWroclaw/app/src/main/java/com/example/user/prototype_smacznywroclaw/SplashActivity.java
@@ -0,0 +1,52 @@
+package com.example.user.prototype_smacznywroclaw;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+
+public class SplashActivity extends ActionBarActivity {
+
+    private static final int CZAS = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // Fullscreen mode
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        // Main actions
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Next Activity starter
+        ActivityStarter starter = new ActivityStarter();
+        starter.start();
+    }
+
+    private class ActivityStarter extends Thread {
+
+        @Override
+        public void run() {
+            try {
+                // Download files from server
+                Thread.sleep(CZAS);
+            } catch (Exception e) {
+                Log.e("SplashScreen", e.getMessage());
+            }
+
+            // Turn on Homepage Activity
+            Intent intent = new Intent(SplashActivity.this, HomepageActivity.class);
+            SplashActivity.this.startActivity(intent);
+            SplashActivity.this.finish();
+        }
+    }
+}
