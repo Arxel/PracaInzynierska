88bfec72c42668fa63dcf7c1582ded0ba52fa448
wojtekmaciej <wojtekmaciej@gmail.com>
2015-03-08 16:33:19 +0100

    Add drawer and search in menu action bar

--- Prototype_SmacznyWroclaw/app/src/main/java/com/example/user/prototype_smacznywroclaw/HomepageActivity.java
+++ Prototype_SmacznyWroclaw/app/src/main/java/com/example/user/prototype_smacznywroclaw/HomepageActivity.java
@@ -1,39 +1,34 @@
 package com.example.user.prototype_smacznywroclaw;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-
+import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class HomepageActivity extends ActionBarActivity {
+    private DrawerLayout drawerLayout;
+    private ListView listView;
+    private String[] functions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-    }
 
+        drawerLayout=(DrawerLayout) findViewById(R.id.drawerLayout);
+        listView=(ListView) findViewById(R.id.drawerList);
+        //listView.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, functions));
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_homepage, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_homepage, menu);
+
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
