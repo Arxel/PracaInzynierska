fda0b108b3e0ef3d5e7c38775a0616dd26b864d8
wojtekmaciej <wojtekmaciej@gmail.com>
2015-03-09 23:28:20 +0100

    Added activities with Google Maps API - simple maps with point,markers and simple maps with drawn trails. All activities are connected to frontend.

--- Prototype_SmacznyWroclaw/app/src/main/java/com/example/user/prototype_smacznywroclaw/HomepageActivity.java
+++ Prototype_SmacznyWroclaw/app/src/main/java/com/example/user/prototype_smacznywroclaw/HomepageActivity.java
@@ -1,27 +1,52 @@
 package com.example.user.prototype_smacznywroclaw;
 
+
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.webkit.WebViewFragment;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class HomepageActivity extends ActionBarActivity {
+    private String[] optionsTitles;
     private DrawerLayout drawerLayout;
-    private ListView listView;
-    private String[] functions;
+    private ListView drawerList;
+    private int selectedPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
-        drawerLayout=(DrawerLayout) findViewById(R.id.drawerLayout);
-        listView=(ListView) findViewById(R.id.drawerList);
-        //listView.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, functions));
+        optionsTitles = getResources().getStringArray(R.array.Functions);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawerList = (ListView) findViewById(R.id.drawerList);
 
+        drawerList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, optionsTitles));
+
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 1:
+                        Intent trailsIntent = new Intent(HomepageActivity.this,TrailMaps.class);
+                        HomepageActivity.this.startActivity(trailsIntent);
+                    case 3:
+                        Intent mapIntent = new Intent(HomepageActivity.this, MapsActivity.class);
+                        HomepageActivity.this.startActivity(mapIntent);
+                        break;
+                }
+
+            }
+        });
     }
 
     @Override
---
+++ Prototype_SmacznyWroclaw/app/src/main/java/com/example/user/prototype_smacznywroclaw/TrailMaps.java
@@ -0,0 +1,60 @@
+package com.example.user.prototype_smacznywroclaw;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class TrailMaps extends Activity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+/*
+        LatLng remontBar = new LatLng(51.111502, 17.058568);
+        LatLng laDonna = new LatLng(51.111691, 17.061808);
+        LatLng bazylia = new LatLng(51.107224, 17.059366);
+        LatLng cynamonBar = new LatLng(51.111939, 17.065588);
+*/
+
+        LatLng przystan = new LatLng(51.114901, 17.032545);
+        LatLng panorama = new LatLng(51.117285, 17.035851);
+        LatLng karczmaMlynska = new LatLng(51.116007, 17.040783);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(przystan, 13));
+
+/*
+        map.addMarker(new MarkerOptions().position(remontBar).snippet("Najlepszy bar we Wrocławiu").title("Remont Bar"));
+        map.addMarker(new MarkerOptions().position(laDonna).snippet("Najlepsza restauracja we Wrocławiu").title("La Donna Cannone"));
+        map.addMarker(new MarkerOptions().position(bazylia).snippet("Jedyny taki studencki bar we Wrocławiu").title("Bazylia"));
+        map.addMarker(new MarkerOptions().position(cynamonBar).snippet("Bar z licencją na włoskie gotowanie").title("Cynamon Bar"));
+*/
+
+        map.addMarker(new MarkerOptions().position(przystan).snippet("Kuchnia śródziemnomorska").title("Karczma Przystań"));
+        map.addMarker(new MarkerOptions().position(panorama).snippet("Restauracja hotelu").title("Restauracja Panorama"));
+        map.addMarker(new MarkerOptions().position(karczmaMlynska).snippet("Restauracja w hotelu Tumskim").title("Karczma Młyńska"));
+
+
+        map.addPolyline(new PolylineOptions().geodesic(true)
+                .add(przystan)
+                .add(panorama)
+                .add(karczmaMlynska)
+        );
+
+        map.setMyLocationEnabled(true);
+
+    }
+}
